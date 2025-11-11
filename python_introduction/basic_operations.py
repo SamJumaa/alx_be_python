853020cae819c34e3b84eef99e528425deb42bf9
Samy Juma <info@samjuma.co.ke>
2025-11-11 23:10:16 +0300

    Performed addition,differences , and product and two numbers

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,16 @@
+number1 = 10
+number2 = 5
+#Perform  arithmetic operations
+sum_results = number1 + number2
+difference_results = number1 - number2
+product_results = number1 * number2
+#Print results of each operation
+print(f"the sum of {number1} and {number2} is {sum_results}")
+print(f"the difference of {number1} and {number2} is {difference_results}")
+print(f"the product of {number1} and {number2} is {product_results}")
+
+
+
+
+
+
