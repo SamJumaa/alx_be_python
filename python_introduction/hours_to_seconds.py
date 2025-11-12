1f9114d024b5ec88f88917a8e680a6eb394c655e
Samy Juma <info@samjuma.co.ke>
2025-11-12 22:37:45 +0300

    corrected the f -string on line 6

--- python_introduction/hours_to_seconds.py
+++ python_introduction/hours_to_seconds.py
@@ -3,4 +3,4 @@ hours = 2
 # Calculate the number of seconds in the 2 hours
 seconds=hours*3600  #since one hour is equal to 3600 seconds
 # print the results
-print({f"hours} hour(s) is {seconds} seconds.")
+print(f"{hours} hour(s) is {seconds} seconds.")
