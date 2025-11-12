acb490ed5e7e2059d87ba8bbcb24b1eff92d1531
Samy Juma <info@samjuma.co.ke>
2025-11-12 23:34:23 +0300

    put  string length *width into brackets

--- python_introduction/rectangle_area.py
+++ python_introduction/rectangle_area.py
@@ -1,6 +1,6 @@
 # Define variables
-length=10  # length of the rectangle
-width=5  # width of the rectangle
+length=10
+width=5  
 # Calculate the area of the rectangle
-area=length*width
+area=(length*width)
 print(f"the area of the rectangle is: {area}")
