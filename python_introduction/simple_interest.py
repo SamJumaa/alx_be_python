01d5d47dcfb9b8267f8589dd46bc29fb7ebed5e6
Samy Juma <info@samjuma.co.ke>
2025-11-12 22:10:44 +0300

    Calculated simple interest earned on an investment over 3 years

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,10 @@
+# Defining variables
+principal=1000 # representing $1000 
+rate=0.05 # representing 5% annual interest rate
+time=3 # representing 3 years
+# Calculation of simple interest 
+
+# formula for simple interest
+interest = principal * rate * time
+print(f"The simple interest is: {interest}")
+
