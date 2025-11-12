ca10b9de3ef463c0880daa02d4741faf2acb5903
Samy Juma <info@samjuma.co.ke>
2025-11-12 22:59:59 +0300

    Calculated user's  age in 2050

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,7 @@
+# Ask the user for their current age
+current_age=int(input("how old are you? "))
+# Calculating the age of the user in the year 2050
+current_year=2023
+future_year=2050
+age= current_age + 27
+print(f"in 2050,you will be {age} years old")
