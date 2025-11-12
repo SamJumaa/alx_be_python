46c6fb18967b4a83b4035c22156d13bb43ed5e01
Samy Juma <info@samjuma.co.ke>
2025-11-12 23:39:04 +0300

    corrected   variable  name on number 5

--- python_introduction/rectangle_area.py
+++ python_introduction/rectangle_area.py
@@ -1,6 +1,6 @@
 # Define variables
-length=10
-width=5  
+length = 10
+width = 5
 # Calculate the area of the rectangle
-area=(length*width)
-print(f"the area of the rectangle is: {area}")
+rectangle_area=(length*width)
+print(f"the area of the rectangle is: {rectangle_area}")
