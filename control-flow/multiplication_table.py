8d13cd4e6e82818cbece291863147ff6a42e8bb9
Samy Juma <info@samjuma.co.ke>
2025-11-17 23:34:52 +0300

    add iterating statment on line 6

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -3,5 +3,6 @@ number = int(input(" Enter a number to see its multiplication table: "))
 #Generate and Print the Multiplication Table
 for i in range(1,11):
     result = number * 1
+ #for loop iterating from 1 to 10   
     print("{} * {} = {}".format(number,i,result))
 
