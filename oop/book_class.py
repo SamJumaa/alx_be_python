f7539e0be1d0165099798459e419ba9b8018acfe
Samy Juma <info@samjuma.co.ke>
2025-12-09 01:13:34 +0300

    add book class script

---
+++ oop/book_class.py
@@ -0,0 +1,23 @@
+class Book:
+
+    def __init__(self, title, author, year):
+        """Constructor to initialize a Book instance."""
+        self.title = title
+        self.author = author
+        self.year = year
+        
+    def __del__(self):
+        """Destructor to announce when a Book instance is deleted."""
+        print(f"Deleting {self.title}")
+    
+    def __str__(self):
+        """String representation for end-user."""
+        return f"{self.title} by {self.author}, published in {self.year}"
+
+    def __repr__(self):
+        """Official representation to recreate the object."""
+        return f"Book('{self.title}', '{self.author}', {self.year})"
+
+
+        
+    
