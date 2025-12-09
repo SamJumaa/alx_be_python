fa9631ba1d066c021e16a6e1e831ca3351d785de
Samy Juma <info@samjuma.co.ke>
2025-12-09 22:58:45 +0300

    add library script

---
+++ oop/library_system.py
@@ -0,0 +1,46 @@
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+
+    def __str__(self):
+        return f"book {self.title } by {self.author}"
+    
+    #creating Ebook child class
+
+class Ebook(Book):
+    def __init__(self, title, author, file_size: int):
+        super().__init__(title, author)   ## Call base class constructor
+        self.file_size = file_size
+
+     def __str__(self):
+        return f"EBook: {self.title} by {self.author} | File Size: {self.file_size} MB"
+        
+ #creating Printbook child class
+
+class PrintBook(Book):
+    def __init__(self, title, author, page_count :int):
+        super().__init__(title, author)  # # Call base class constructor
+        self.page_count = page_count
+
+        return f"PrintedBook: {self.title} by {self.author} |pages: {self.page_count}"
+# Composition Class: Library
+
+class Library:
+    def __init__(self):
+        self.books = []   # List to store books (composition)
+        
+    def add_book(self, book):
+        self.books.append(book)
+
+    def list_books(self):
+        print("\n--- Library Collection ---")
+        for book in self.books:
+            print(book)
+
+  
+       
+
+    
+
+#usage
